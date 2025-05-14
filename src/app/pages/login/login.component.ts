5ab888ac73b943fc38567cddd942006097b08904
Vítor Aguiar <v.aguiar@ba.estudante.senai.br>
2025-05-14 19:41:07 -0300

    login completo

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { SaudacaoComponent } from '../../components/saudacao/saudacao.component';
 
 @Component({
   selector: 'app-login',
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent,SaudacaoComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
